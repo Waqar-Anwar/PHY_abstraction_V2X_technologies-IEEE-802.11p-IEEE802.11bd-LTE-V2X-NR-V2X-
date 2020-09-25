098545c97e82e3ce430eb3f1c9aad48b13220f07
Waqar-Anwar <71836405+Waqar-Anwar@users.noreply.github.com>
2020-09-25 16:37:15 +0200

    Add files via upload

--- NR-V2X/Datarate_and_latency_calculations_for_NR_V2X.m
+++ NR-V2X/Datarate_and_latency_calculations_for_NR_V2X.m
@@ -1,9 +1,11 @@
 % Currently these results are based on the PUSCH performmance with OFDM which uses almost simialr 
 % PHY configration as defined by PSSCH. 
 
+% Please Download first Results_nr_awgn.mat from GitHub and channel_data.mat from (https://www.dropbox.com/s/tx975gatia41mq1/channel_data.mat?dl=0)
+
 b = load('Results_nr_awgn.mat'); % AWGN look-up table
 chan = load('channel_data.mat'); % Channel gain recorded for 1000 channel relizations of Uran-crossing NLOS channel [M. Kahn, “V2V radio channel models,” IEEE 802.11-14/0259r0, Feb.2014] using PDSCH MATLAB 5G toolbox example [https://de.mathworks.com/help/5g/ug/nr-pdsch-throughput.html]
-channel= chan.hest(1:1000,chan.puschIndices); % Indexes on which data is transmitted puschIndices
+channel= chan.hest(1:1000,:,2:13); % Symbols used to transmit data
 
 P_tx = 23; % transmitted power
 P_L0 = 47.86; % path loss at reference distance 
@@ -40,20 +42,21 @@ for i=1:length(snr_d)
     
     pn = 10^(-snr_d(i)/10);
     ICI_plus_noise = (ICI + pn)/sigma_sym;
-
     
-      estimat = reshape(channel,[length(channel(:,1)),length(channel(1,:))/10,10]); % 1000 channel realizations
-      sigma_mmse(:,:)= mean(((abs(estimat).^2)./((abs(estimat).^2)+ ICI_plus_noise)).^2,2);
-      snr_real = 1./(1./(sqrt(sigma_mmse))-1);  % SINR per received OFDM symbol
+    snr_real = (abs(channel).^2) ./ICI_plus_noise;
+    
+% %       estimat = reshape(channel,[length(channel(:,1)),length(channel(1,:))/12,12]); % 1000 channel realizations
+%       sigma_mmse= mean(((abs(channel).^2)./((abs(channel).^2)+ ICI_plus_noise)).^2,2);
+%       snr_real = 1./(1./(sqrt(sigma_mmse))-1);  % SINR per received OFDM symbol
     
     for ind =1:mcs_len
         
         snr_awgn = b.snr_nr_awgn(ind,:);    % AWGN SINR table
         per_awgn = b.per_nr_awgn(ind,:);    % AWGN PER
         
-        snr_ieesm=10.*log10((beta(ind)/2).*(lambertw(exp(1).*((sum(exp(-snr_real'./beta(ind))./sqrt(((2.*snr_real')./beta(ind)) + 1))./10).^(-2))) - 1));  % effective SINR calculations
+        snr_ieesm=10*log10((beta(ind)/2).*(lambertw(exp(1).*(mean(exp(-snr_real(:,1:(12*n_RB_NR(ind)),:)./beta(ind))./sqrt(((2.*snr_real(:,1:(12*n_RB_NR(ind)),:))./beta(ind)) + 1),[2 3]).^(-2))) - 1)); %Effective SINR mapping for used number of Resource blocks
         snr_ieesm(isinf(snr_ieesm)) = 100; %In case of high SINR matlab return +infinity and this replace it with some logical value
-        loc_d = knnsearch(snr_awgn',snr_ieesm'); % closest AWGN simulated points
+        loc_d = knnsearch(snr_awgn',snr_ieesm); % closest AWGN simulated points
         per = per_awgn(loc_d);  %PER against AWGN SNR
         data_rate(ind,:) = (1-per)*Gamma_NR(ind);  % data rate calcuation for the considered MCS
         latency_temp(ind,:)= t_tx_NR(ind)./((1-per)+ 0.0001); % data rate calcuation for the considered MCS and 0.0001 is just used to avoid infinity
