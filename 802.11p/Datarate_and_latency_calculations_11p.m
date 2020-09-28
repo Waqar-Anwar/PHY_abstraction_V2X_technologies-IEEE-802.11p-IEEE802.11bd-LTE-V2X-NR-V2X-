77dde20baa6c724361056d354105177a85defd02
Waqar-Anwar <71836405+Waqar-Anwar@users.noreply.github.com>
2020-09-28 13:57:44 +0200

    Add files via upload

--- 802.11p/Datarate_and_latency_calculations_11p.m
+++ 802.11p/Datarate_and_latency_calculations_11p.m
@@ -1,5 +1,10 @@
-b = load('Results_11bd_awgn.mat');  % AWGN reference of 11bd cueves for first 10 MCS (0-9)
-chan = load('channel_11bd.mat'); % fading channel
+% This script could be used to simulate Data rate and transmission latency
+% for 802.11p
+
+% Please Download first Results_11p_awgn.mat from GitHub and channel_11p.mat from (which can be downloaded here: https://www.dropbox.com/s/98lxwzqvzzoxp9l/channel_11p.mat?dl=0)
+
+b = load('Results_11p_awgn.mat');  % AWGN reference cueves of 11p
+chan = load('channel_11p.mat'); % fading channel
 channel = chan.hest(1:1000,:,:); % first 1000 channel relaizations
 
 
@@ -13,25 +18,21 @@ G_rx = 3;% receive antenna gain
 Pb = 300; % Number of bytes in a packet
 
 
-n_dc_11bd = 52; % number of data carriers
-r_11bd = [1/2 1/2 3/4 1/2 3/4 2/3 3/4 5/6 3/4 5/6]; % code rate of first 10 MCSs (0-9)
-n_bps = [1 2 2 4 4 6 6 6 8 8]; %number of bits transmitted by each modulation schemes for first 10 MCSs (0-9)
-t_ma = [8 8 8 8 8 4 4 4 4 4]; % midambel periodicity
-t_pre_11bd = 80e-6; % preamble duratation
+n_dc_11p = 48; % number of data carriers
+r_11p = [1/2 3/4 1/2 3/4 1/2 3/4 2/3 3/4]; % code rate of 11p MCSs
+n_bps = [1 1 2 2 4 4 6 6]; %number of bits transmitted by each modulation schemes of 11p MCSs
+t_pre_11p = 40e-6; % preamble duratation
 t_AIFS = 32e-6; % arbitrary inter-frame space
 t_sym = 8e-6; % OFDM symbol duration
+beta = [1 1 2 2 10 10 42 42]; % beta values depending on the modulation order 1 for BPSK, 2 for QPSK, 10 for 16 QAM, 42 for 64 QAM
 
 
 
-n_sym_11bd = ceil((Pb*8)./(n_dc_11bd.*r_11bd.*n_bps)); % number of data symbols 
-n_ma = floor((n_sym_11bd-1)./t_ma); % number of midambles
-t_tx_11bd = (t_pre_11bd + t_AIFS + t_sym.*n_sym_11bd + t_sym.*n_ma)*1e3; % transmission latency of each MCS in ms
-Gamma_11bd = (Pb*8 ./t_tx_11bd)/1e3; % Data rates of each MCS in Mbps
+n_sym_11p = ceil((Pb*8)./(n_dc_11p.*r_11p.*n_bps)); % number of data symbols 
+t_tx_11p = (t_pre_11p + t_AIFS + t_sym.*n_sym_11p)*1e3; % transmission latency of each MCS in ms
+Gamma_11p = (Pb*8 ./t_tx_11p)/1e3; % Data rates of each MCS in Mbps
 
-beta = [1 2 2 10 10 42 42 42 170 170]; % beta values depending on the modulation order 1 for BPSK, 2 for QPSK, 10 for 16 QAM, 42 for 64 QAM, 170 for 256 QAM
-% data_rate_mcs = [2.31 4.17 5.77 6.98 9.09 11.11 11.54 12.00 13.64 13.64];
-% latency = [1.040 0.576 0.416 0.344 0.264 0.216 0.208 0.200 0.176 0.176];
-mcs_len = 10; % number of MCSs
+mcs_len = 8; % number of MCSs
 
 
 
@@ -48,29 +49,27 @@ clear data_rate latency_temp
 for i=1:length(snr_d)
     
     pn = 10^(-snr_d(i)/10);
-    noise = (ICI + pn)/sigma_sym;
+    ICI_plus_noise = (ICI + pn)/sigma_sym;
     
-    snr_real = 1./((1./((abs(channel).^2)./((abs(channel).^2)+ noise)))-1); % received SINR per each data symbols and sub-carrier
+    snr_real = (abs(channel).^2) ./ICI_plus_noise; % received SINR per sub-carrier of each OFDM symbol
     
     for ind =1:mcs_len
         
-        snr_awgn = b.snr_11bd_awgn(ind,:);
-        per_awgn = b.per_11bd_awgn(ind,:);
+        snr_awgn = b.snr_11p_awgn(ind,:);
+        per_awgn = b.per_11p_awgn(ind,:);
         
-%        snr_ieesm=10.*log10((beta(ind)/2).*(lambertw(exp(1).*((sum(exp(-snr_real(:,:,1:n_sym_11bd(ind))'./beta(ind))./sqrt(((2.*snr_real')./beta(ind)) + 1))./52).^(-2))) - 1));
-        snr_ieesm=10*log10((beta(ind)/2).*(lambertw(exp(1).*(mean(exp(-snr_real(:,:,1:n_sym_11bd(ind))./beta(ind))./sqrt(((2.*snr_real(:,:,1:n_sym_11bd(ind)))./beta(ind)) + 1),[2 3]).^(-2))) - 1)); %Effective SINR mapping
+        snr_ieesm=10*log10((beta(ind)/2).*(lambertw(exp(1).*(mean(exp(-snr_real(:,:,1:n_sym_11p(ind))./beta(ind))./sqrt(((2.*snr_real(:,:,1:n_sym_11p(ind)))./beta(ind)) + 1),[2 3]).^(-2))) - 1)); %Effective SINR mapping
         snr_ieesm(isinf(snr_ieesm)) = 100;  % in case of positive infinity it will replace the value with 100
         loc_d = knnsearch(snr_awgn',snr_ieesm);
         per = per_awgn(loc_d);
-        data_rate(ind,:) = (1-per)*Gamma_11bd(ind);  % effective data rate
-        latency_temp(ind,:)=t_tx_11bd(ind)./((1-per)+0.0001); % transmission latency of selected mcs 0.0001 is used to avoid infinty in case of 1-per is 0
+        data_rate(ind,:) = (1-per)*Gamma_11p(ind);  % effective data rate
+        latency_temp(ind,:)=t_tx_11p(ind)./((1-per)+0.0001); % transmission latency of selected mcs 0.0001 is used to avoid infinty in case of 1-per is 0
         
     end
     
-    data_rate_11bd(i) = mean(max(data_rate)); % Select the MCS which deliver maximum data rates
-    t_IAT_11bd(i)= mean(min(latency_temp)); % Select the MCS provide Minimum latency
-
-    
+    data_rate_11p(i) = mean(max(data_rate)); % Select the MCS which deliver maximum data rates
+    t_IAT_11p(i)= mean(min(latency_temp)); % Select the MCS provide Minimum latency
+ 
 end
 
 
